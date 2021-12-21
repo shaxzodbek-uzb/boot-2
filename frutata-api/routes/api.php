a3c4a13ae12e974aa2d4a8354adf52aef3fdec30
ismile <voyasa9172@latovic.com>
2021-12-21 09:56:22 +0500

    product category crud

--- frutata-api/routes/api.php
+++ frutata-api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::resource('products', ProductController::class);
+Route::resource('product-categories', ProductCategoryController::class);
